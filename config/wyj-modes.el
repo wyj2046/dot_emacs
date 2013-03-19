08d70c49b3e52e2122d98d24d494322a3abe52e1
wyj2046 <wyj2046@gmail.com>
2013-03-19 20:30:15 +0800

    参考sacha chua的文章后，发现貌似还是php-mode爽些，在wyj-modes中增加了Drupal风格的PHP缩进模式

--- config/wyj-modes.el
+++ config/wyj-modes.el
@@ -66,6 +66,18 @@
 (autoload 'php-mode "php-mode" "Major mode for editing php code." t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
-
+;; 参考网址http://sachachua.com/blog/2008/07/emacs-and-php-tutorial-php-mode/
+;; 增加了Drupal风格的PHP缩进模式
+(defun wicked/php-mode-init ()
+  "Set some buffer-local variables."
+  (setq case-fold-search t) 
+  (setq indent-tabs-mode nil)
+  (setq fill-column 78)
+  (setq c-basic-offset 2)
+  (c-set-offset 'arglist-cont 0)
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'case-label 2)
+  (c-set-offset 'arglist-close 0))
+(add-hook 'php-mode-hook 'wicked/php-mode-init)
 
 (provide 'wyj-modes)
