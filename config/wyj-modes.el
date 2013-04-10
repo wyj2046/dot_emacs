08d5ac8b0c2c258d4a0579fb36e40fd45b0cc49f
wyj2046 <wyj2046@gmail.com>
2013-04-10 13:44:14 +0800

    为了配合项目需要,将php缩进模式从2改为4

--- config/wyj-modes.el
+++ config/wyj-modes.el
@@ -73,7 +73,7 @@
   (setq case-fold-search t) 
   (setq indent-tabs-mode nil)
   (setq fill-column 78)
-  (setq c-basic-offset 2)
+  (setq c-basic-offset 4)
   (c-set-offset 'arglist-cont 0)
   (c-set-offset 'arglist-intro '+)
   (c-set-offset 'case-label 2)
